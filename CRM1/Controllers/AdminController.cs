9cd6393011cfc7747564a275d26f3bd477edb254
SiddhivinayakDhangonde <s.dhangonde@gmail.com>
2025-12-24 13:13:08 +0530

    Add project files.

---
+++ CRM1/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRM1.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AdminLogin()
+        {
+            return View();
+        }
+    }
+}
